e2b6dc25ecef0858bff1226c31a243171c271654
Copeland Carter <smartycope@gmail.com>
2025-07-31 17:42:58 -0500

    Remove scraping code

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,21 +1,15 @@
-import streamlit as st
-from streamlit import session_state as ss
-from datetime import datetime as dt
-from datetime import timedelta
-import requests
-import json
-import narwhals as nw
-import pandas as pd
-import numpy as np
-
-# Configure Narwhals to use pandas as the backend
-# nw.config.set_backend('pandas')
-import pickle
-
-from cryptography.fernet import Fernet
 import base64
 import hashlib
+from datetime import datetime as dt
+from datetime import timedelta
+
+import narwhals as nw
+import numpy as np
+import pandas as pd
 import pprp
+import streamlit as st
+from cryptography.fernet import Fernet
+from streamlit import session_state as ss
 
 password_hash = {
     'salt': 'yITsoJop3O7QiNnzSdeTSA==',
@@ -26,7 +20,6 @@ password_hash = {
 now = dt.now().strftime("%Y-%m-%d")
 search_url = "https://workspace.refinitiv.com/api/tm3-backend/muni-data-analysis/deal-search/search"
 detail_url = 'https://workspace.refinitiv.com/api/tm3-backend/muni-data-analysis/common/deal-analysis?dealId={deal_id}&evaluationDate='+now
-# TM3_URL = "https://workspace.refinitiv.com/muni-data-analysis/deal-search/deal/{deal_id}"
 TM3_DEAL_URL = "https://workspace.refinitiv.com/web/rap/tm3-app/muni-data-analysis/deal-search/details/{deal_id}/deal-analysis?evaluationDate="+now
 TM3_CUSIP_URL = 'https://workspace.refinitiv.com/web/rap/tm3-app/muni-data-analysis/cusip-search/details/{cusip}/main?evaluationDate=' + now
 EMMA_URL = 'https://emma.msrb.org/QuickSearch/Results?quickSearchText={cusip}'
@@ -37,7 +30,6 @@ if 'detail_log' not in ss: ss.detail_log = []
 if 'search_log_data' not in ss: ss.search_log_data = {}
 # Indexed by deal_id
 if 'detail_log_data' not in ss: ss.detail_log_data = {}
-
 
 
 def verify_password(password: str, stored: dict):
@@ -89,54 +81,6 @@ if True:
 def get_preloaded_data():
     return decrypt_file_to_df('backup_data/bonds_encrypted.bin', st.session_state["authenticated"]), decrypt_file_to_df('backup_data/coupons_encrypted.bin', st.session_state["authenticated"])
 
-
-
-
-    # search button
-    # if st.button("Scrape"):
-    #     if len(cookie) < 50:
-    #         st.error("Invalid cookie: ask Cope needs to get you a new one")
-    #         st.stop()
-    #     try:
-    #         schools = get_schools(from_date, to_date)
-    #         coupons, bonus_school_data = get_deals(schools['dealId'].to_list())
-
-    #         try:
-    #             additional_data = schools.select('first_coupon_date', 'first_call_date', 'security_type', 'bank_qualified', 'sp_rating', 'bond_insurance', 'paying_agent', 'deal_id').rename({'deal_id': 'dealId'})
-    #         except Exception as e:
-    #             bonds = schools.unique('dealId')
-    #         else:
-    #             bonds = schools.unique('dealId').join(additional_data, on='dealId', how='left')
-    #         show_target(bonds, coupons)
-
-    #     except Exception as e:
-    #         st.error('Something went wrong. Please download this file and give it to Cope:')
-    #         # TODO: delete this later
-    #         st.exception(e)
-    #         # Pickle everything and stuff it in a single zip file
-    #         st.download_button("Download logs",
-    #             data=pickle.dumps({
-    #                 'from_date': from_date,
-    #                 'to_date': to_date,
-    #                 'cookie': cookie,
-    #                 'error': e,
-    #                 'error_str': str(e),
-    #                 'search_log': ss.search_log,
-    #                 'detail_log': ss.detail_log,
-    #                 'search_log_data': ss.search_log_data,
-    #                 'detail_log_data': ss.detail_log_data
-    #             }),
-    #             file_name=f"log-{now}.pkl"
-    #         )
-
-    #         # """ Search logs: """
-    #         # st.write(ss.search_log)
-
-    #         # st.write(ss.detail_log)
-    #         # st.write(ss.search_log_data)
-    #         """ Data gathered so far: """
-            # st.write(ss.detail_log_data)
-
 bonds, coupons = get_preloaded_data()
 @st.cache_data
 def get_target():
@@ -158,9 +102,6 @@ def get_target():
         TM3_url=nw.col('dealId').map_batches(lambda x: np.array(TM3_DEAL_URL.format(deal_id=i) for i in x)),
         TM3_cusip_url=nw.col('cusip').map_batches(lambda x: np.array(TM3_CUSIP_URL.format(cusip=i) for i in x)),
         emma_url=nw.col('cusip').map_batches(lambda x: np.array(EMMA_URL.format(cusip=i) for i in x)),
-        # TM3_url=nw.col('dealId').str.replace(r'.*', TM3_DEAL_URL.format(deal_id='$1')),
-        # TM3_cusip_url=nw.col('cusip').str.replace(r'.*', TM3_CUSIP_URL.format(cusip='$1')),
-        # emma_url=nw.col('cusip').str.replace(r'.*', EMMA_URL.format(cusip='$1')),
     )
 
     # Calculate total par amount per deal
@@ -197,146 +138,13 @@ def get_target():
 
     return df
 
-def search_req_body(saleDateFrom, saleDateTo):
-    return {
-        "saleDateFrom": saleDateFrom.strftime("%Y-%m-%d"),
-        "saleDateTo": saleDateTo.strftime("%Y-%m-%d"),
-        "evaluationDate": now,
-        "either": False,
-        "both": True,
-        "moodyLong": True,
-        "moodyShort": False,
-        "sandpLong": True,
-        "sandpShort": False,
-        "moodyRatingFrom": {
-            "value": "UR",
-            "label": "UR"
-        },
-        "moodyRatingTo": {
-            "value": "Aaa",
-            "label": "Aaa"
-        },
-        "moodyUnderlyingFrom": {
-            "value": "UR",
-            "label": "UR"
-        },
-        "moodyUnderlyingTo": {
-            "value": "Aaa",
-            "label": "Aaa"
-        },
-        "sandpRatingFrom": {
-            "value": "UR",
-            "label": "UR"
-        },
-        "sandpRatingTo": {
-            "value": "AAA",
-            "label": "AAA"
-        },
-        "sandpUnderlyingFrom": {
-            "value": "UR",
-            "label": "UR"
-        },
-        "sandpUnderlyingTo": {
-            "value": "AAA",
-            "label": "AAA"
-        },
-        "useOfProceeds": {
-            "label": "EDUC Charter school",
-            "value": "E5"
-        },
-        "cusips": [],
-        "isEntitledToSp": False,
-        "isAllBondInsurance": False,
-        "isAllIssueType": False
-    }
-
-def reset_logs():
-    ss.search_log = []
-    ss.detail_log = []
-    ss.search_log_data = {}
-    ss.detail_log_data = {}
-
-def search(from_date, to_date, _log=True, tabs=0):
-    print(f'{"\t" * tabs}Searching {from_date} to {to_date}...')
-    # resp = requests.post(search_url, headers={'cookie': cookie}, data=search_req_body(from_date, to_date))
-    resp = requests.post(search_url, headers={'cookie': cookie}, json=search_req_body(from_date, to_date))
-    print(f'{"\t" * tabs}...finished searching with {resp.status_code}')
-    ss.search_log.append(resp)
-    if resp.status_code == 204:
-        return {'data': [], 'overThreshold': False}
-
-    if resp.status_code != 200:
-        print('!!! Error searching from', from_date, 'to', to_date, ' !!!')
-
-    j = resp.json()
-    # If there's more data, recurse in halfs, and combine the data
-    if j['overThreshold']:
-        print(f'{"\t" * tabs}Over threshold, recursing...')
-        half = from_date + ((to_date - from_date) // 2)
-        half1 = search(from_date, half, _log=False, tabs=tabs+1)
-        half2 = search(half, to_date, _log=False, tabs=tabs+1)
-        j = {'data': half1['data'] + half2['data'], 'overThreshold': False}
-        print(f'{"\t" * tabs}...finished recursing')
-
-    if _log:
-        ss.search_log_data[(from_date, to_date)] = j
-
-    return j
-
-def details(deal_id):
-    print(f'Fetching details for {deal_id}...')
-    resp = requests.get(detail_url.format(deal_id=deal_id), headers={'cookie': cookie})
-    print(f'...finished fetching details with {resp.status_code}')
-    ss.detail_log.append(resp)
-    j = resp.json()
-    ss.detail_log_data[deal_id] = j
-    return j
-
-@st.cache_data
-def get_schools(from_date, to_date):
-    schools = []
-    # Search by month
-    i = from_date
-    while i <= to_date:
-        schools += search(i, i+timedelta(days=30))['data']
-        i += timedelta(days=30)
-    try:
-        return nw.DataFrame(schools)
-    except:
-        return schools
-
-@st.cache_data
-def get_deals(deal_ids):
-    school_bonus = []
-    coupons = []
-    for deal_id in deal_ids:
-        d = details(deal_id)['data']
-
-        dd = d['dealDetails']
-        for i in dd:
-            i['deal_id'] = deal_id
-        school_bonus += dd
-
-        cd = d['cusipDetails']
-        for i in cd:
-            i['deal_id'] = deal_id
-        coupons += cd
-    try:
-        return nw.DataFrame(coupons), nw.DataFrame(school_bonus)
-    except:
-        return coupons, school_bonus
-
 
 target = get_target()
 
 st.title("School Bonds")
-cookie = ''
 with st.sidebar:
-    # cookie = st.text_input("Auth Cookie (ask Cope, only lasts an hour or 2)")
-    # if cookie:
-    #     st.success("Cookie loaded")
     precision = st.number_input("Precision (digits)", value=1, min_value=1, max_value=10, step=1, format='%d')
-    max_date = target['datedDate'].max()
+    max_date = target['saleDate'].max()
     if max_date < dt.now() - timedelta(days=365):
         box = st.error
     elif max_date < dt.now() - timedelta(days=6*30):
@@ -345,12 +153,12 @@ with st.sidebar:
         box = st.success
     l, r = st.columns([2, 1])
     with l:
-        box(f"Data is <{int((max_date - dt.now()).days)} days old")
+        box(f"Data is ~{int((dt.now() - max_date).days)} days old")
     if r.button('Update'):
         st.warning('This button doesn\'t currently do anything, but it will soon!')
 
     f"""
-    Data range: {target['datedDate'].min()} to {max_date.strftime('%Y-%m-%d')}
+    Data range: {target['saleDate'].min().strftime('%Y-%m-%d')} to {max_date.strftime('%Y-%m-%d')}
 
     - To download: there's a download button that shows up over the data as you hover over it
     - To see which columns are hidden: click the eye icon that shows up over the data as you hover over it
@@ -442,7 +250,6 @@ hide_nulls = [
 ]
 
 
-# with st.form("Pre-filtered data"):
 with st.container(border=True):
     l, m, r = st.columns(3)
     state = l.selectbox("State", ['All'] + bonds['state'].unique().sort().to_list())
@@ -474,10 +281,8 @@ with st.container(border=True):
             name = name if isinstance(name, str) else name['label']
             if columns[cnt % 4].checkbox(name, value=col in hide_nulls, key=col):
                 exclude_null.append(col)
-    # l, r = st.columns(2)
     show_coupons = st.checkbox("Show individual coupons", value=False)
     exclude_taxable = st.checkbox("Exclude taxable", value=True)
-    # l.form_submit_button("Submit")
 
 if not show_coupons:
     hide.append('cusip')
@@ -515,10 +320,6 @@ for col in exclude_null:
 st.write(f'`{len(shown)}`/`{len(deals if not show_coupons else target)}` {"schools" if not show_coupons else "coupons"} selected')
 
 shown_df = shown.to_pandas()
-# Convert datetime columns to string for display
-# for col in ['datedDate', 'maturity_date', 'first_coupon_date', 'first_call_date', 'saleDate']:
-#     if col in shown_df.columns and pd.api.types.is_datetime64_any_dtype(shown_df[col]):
-#         shown_df[col] = shown_df[col].dt.strftime('%Y-%m-%d')
 
 # Display the data
 st.dataframe(
